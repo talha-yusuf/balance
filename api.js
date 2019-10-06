77a540e47c8c4d02df6390a9e496bd332244a25c
talha <talha.yusuf@miranz.net>
2019-10-06 11:40:38 +0000

    initial code

---
+++ api.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var express = require('express');
+var app = express();
+
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('https://walletsecuredgoldcoin.com'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({type: 'application/json'}));
+
+
+// Get Balance
+var getBalance = express.Router();
+getBalance.post('/', function(request, res){
+
+    public_key = request.body.public_address;
+    var value;
+    
+    web3.eth.getBalance(public_key)
+    .then(function(balance){
+        value = web3.utils.fromWei(balance, 'ether');
+        console.log(value);
+    });
+
+    res.contentType('application/json');
+    res.end(JSON.stringify(value));
+});
+app.use('/balance', getBalance);
+
+
+if (module === require.main) {
+    // Start the server
+    var server = app.listen(process.env.PORT || 3000, function () {
+        var port = server.address().port;
+        console.log('App listening on port %s', port);
+    });
+  }
+module.exports = app;
